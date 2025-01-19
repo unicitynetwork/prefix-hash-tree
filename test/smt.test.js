bd3d89e388cab8948dc470dcae3055523fd61709
Vladimir Cryptohog Rogojin <vrogojin@vrogojin.net>
2025-01-19 16:16:29 +0200

    unit tests for all paths with length 7

--- test/smt.test.js
+++ test/smt.test.js
@@ -6,9 +6,6 @@ const { hash, wordArrayToHex } = require("../smt/helper.js");
 
 function checkPaths(smt, leafs, pathTransformFunc, shouldBeIncluded, failMsg){
 	for(const leaf of leafs){
-/*		const pl = BigInt(leaf.path.toString(2).length)/2n;
-		const mask = (1n << pl)-1n;
-		const requestedPath = (leaf.path & mask) | (1n << pl);*/
 		const requestedPath = pathTransformFunc(leaf.path);
 		console.log(requestedPath.toString(2));
 		const path = smt.getPath(requestedPath);
@@ -16,70 +13,58 @@ function checkPaths(smt, leafs, pathTransformFunc, shouldBeIncluded, failMsg){
 	}
 }
 
+function generatePaths(l){
+    let leafs = [];
+    const trail = (1n << BigInt(l));
+    for(let i=0n; i<trail; i++)
+	leafs.push({path: i | trail, value: wordArrayToHex(hash('value'+i.toString(2)))});
+    return leafs;
+}
+
 describe("SMT routines", function() {
 
-    const leafs = [
-	{path: 0b100000000n, value: wordArrayToHex(hash('value00000000'))}, 
-	{path: 0b100010000n, value: wordArrayToHex(hash('value100010000'))}, 
-	{path: 0b111100101n, value: hash('value11100101')}, 
-	{path:      0b1100n, value: hash('value100')}, 
-	{path:      0b1011n, value: hash('value011')},
-	{path: 0b111101111n, value: wordArrayToHex(hash('value11101111'))}, 
-	{path:  0b10001010n, value: hash('value0001010')}, 
-	{path:  0b11010101n, value: hash('value1010101')}
-    ];
+    for(let i=0; i<2; i++){
 
-    let smt;
+	context(i==0?"sparse tree":"filled tree", function() {
+	    const leafs = i==0?[
+		{path: 0b100000000n, value: wordArrayToHex(hash('value00000000'))}, 
+		{path: 0b100010000n, value: wordArrayToHex(hash('value100010000'))}, 
+		{path: 0b111100101n, value: hash('value11100101')}, 
+		{path:      0b1100n, value: hash('value100')}, 
+		{path:      0b1011n, value: hash('value011')},
+		{path: 0b111101111n, value: wordArrayToHex(hash('value11101111'))}, 
+		{path:  0b10001010n, value: hash('value0001010')}, 
+		{path:  0b11010101n, value: hash('value1010101')}
+	    ]:generatePaths(7);
 
-    beforeEach(function(){
-	smt = new SMT(hash, leafs);
-    });
+    	    let smt;
 
-    context("extracting proofs", function() {
+    	    beforeEach(function(){
+		smt = new SMT(hash, leafs);
+	    });
 
-	it("extracting all inclusion proofs", function() {
-/*	    for(const leaf of leafs){
-		console.log(leaf.path.toString(2));
-		const path = smt.getPath(leaf.path);
-		assert.equal(includesPath(hash, leaf.path, path), true, "Leaf at location "+leaf.path.toString(2)+" not included");
-	    }*/
-	    checkPaths(smt, leafs, (p) => {return p;}, true, (p) => {return "Leaf at location "+p.toString(2)+" not included";});
+	    context("extracting proofs", function() {
+
+		it("extracting all inclusion proofs", function() {
+		    checkPaths(smt, leafs, (p) => {return p;}, true, (p) => {return "Leaf at location "+p.toString(2)+" not included";});
+		});
+
+		if(i==0)it("extracting non-inclusion proofs for paths deviating from the existing branches", function() {
+		    checkPaths(smt, leafs, (p) => {return p ^ 4n;}, false, (p) => {return "Leaf at location "+p.toString(2)+" included";});
+		});
+
+		it("extracting non-inclusion proofs for paths exceeding existing branches", function() {
+		    checkPaths(smt, leafs, (p) => {return p | 1024n;}, false, (p) => {return "Leaf at location "+p.toString(2)+" included";});
+		});
+
+		it("extracting non-inclusion proofs for paths stopping inside existing branches", function() {
+		    checkPaths(smt, leafs, (p) => {const pl = BigInt(p.toString(2).length)/2n; const mask = (1n << pl)-1n; return (p & mask) | (1n << pl);}, false, (p) => {return "Leaf at location "+p.toString(2)+" included";});
+		});
+
+	    });
+
 	});
 
-	it("extracting non-inclusion proofs for paths deviating from the existing branches", function() {
-/*	    for(const leaf of leafs){
-		const requestedPath = leaf.path ^ 4n;
-		console.log(requestedPath.toString(2));
-		const path = smt.getPath(requestedPath);
-		assert.equal(includesPath(hash, requestedPath, path), false, "Leaf at location "+requestedPath.toString(2)+" not included");
-	    }*/
-	    checkPaths(smt, leafs, (p) => {return p ^ 4n;}, false, (p) => {return "Leaf at location "+p.toString(2)+" included";});
-	});
-
-	it("extracting non-inclusion proofs for paths exceeding existing branches", function() {
-/*	    for(const leaf of leafs){
-		const requestedPath = leaf.path | 1024n;
-		console.log(requestedPath.toString(2));
-		const path = smt.getPath(requestedPath);
-		assert.equal(includesPath(hash, requestedPath, path), false, "Leaf at location "+requestedPath.toString(2)+" not included");
-	    }*/
-	    checkPaths(smt, leafs, (p) => {return p | 1024n;}, false, (p) => {return "Leaf at location "+p.toString(2)+" included";});
-	});
-
-	it("extracting non-inclusion proofs for paths stopping inside existing branches", function() {
-/*	    for(const leaf of leafs){
-		const pl = BigInt(leaf.path.toString(2).length)/2n;
-		const mask = (1n << pl)-1n;
-		const requestedPath = (leaf.path & mask) | (1n << pl);
-		console.log(requestedPath.toString(2));
-		const path = smt.getPath(requestedPath);
-		assert.equal(includesPath(hash, requestedPath, path), false, "Leaf at location "+requestedPath.toString(2)+" included");
-	    }*/
-	    checkPaths(smt, leafs, (p) => {const pl = BigInt(p.toString(2).length)/2n; const mask = (1n << pl)-1n; return (p & mask) | (1n << pl);}, false, (p) => {return "Leaf at location "+p.toString(2)+" included";});
-	});
-
-
-
-    });
+    }
 
 });
